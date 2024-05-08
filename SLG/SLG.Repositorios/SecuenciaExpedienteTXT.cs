0bcfb0ed1c7a3e050463ef6ead96f9f30ba8fdd3
felidira <nahuel500kapo@gmail.com>
2024-05-08 19:35:57 -0300

    update 4

---
+++ SLG/SLG.Repositorios/SecuenciaExpedienteTXT.cs
@@ -0,0 +1,16 @@
+namespace SLG.Repositorios;
+
+public class SecuenciaExpedienteTXT
+{
+    readonly string nombreArch="SecuenciaExp.txt";
+
+    public int LeerID()
+    {
+        using StreamReader sr= new StreamReader(nombreArch);
+        int auxID=int.Parse(sr.ReadLine() ?? "-1"); // try catch para verificar si ya existe secuenciaexp.txt, si no existe crear
+        using StreamWriter sw = new StreamWriter(nombreArch);
+        auxID++;
+        sw.WriteLine(auxID.ToString(),false);
+        return auxID;
+    }
+}
